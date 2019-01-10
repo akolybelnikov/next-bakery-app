1f7751fa71e601d97c532b17287730f714aca77c
akolybelnikov <a.kolybelnikov@gmail.com>
2019-01-10 23:28:39 +0100

    with exportPathmap

--- next.config.js
+++ next.config.js
@@ -62,6 +62,16 @@ const nextConfig = {
     defaultUserEmail: process.env.DEFAULT_USER_EMAIL,
     defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
   },
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/user': { page: '/user' },
+      '/authenticate': { page: '/authenticate' },
+      '/contact': { page: '/contact' },
+      '/offers': { page: '/offers' },
+      '/assortment': { page: '/assortment' },
+    }
+  },
 }
 
 module.exports = withPlugins(
